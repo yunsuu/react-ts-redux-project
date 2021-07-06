8d5c3ddd3abc2a93af42fa8e4ecc276e98b1775a
yunsuu <ljsql934@gmail.com>
2021-07-07 00:32:46 +0900

    working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { ChakraProvider,Button } from "@chakra-ui/react"
+import {
+  ChakraProvider,
+  VStack,
+  StackDivider,
+  Button,
+  Stack,
+  Heading,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 // import './App.css';
+
+// 영상 카드 만들기
+// nav 만들기
+// 레시피 만들기
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider>
-      s
-      <Button colorScheme="blue">Button</Button>
-    </ChakraProvider>
+        <VStack
+          divider={<StackDivider borderColor="gray.200" />}
+          spacing={4}
+          align="stretch"
+        >
+          <Box p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl"> 앙 </Heading>
+            <Text mt={4}>잉이이이이잉</Text>
+          </Box>
+        </VStack>
+      </ChakraProvider>
     </div>
   );
 }
